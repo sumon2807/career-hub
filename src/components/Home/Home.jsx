88689f2fda643f52a916728155e158d90255c8c4
sumon2807 <sharifulbd911@gmail.com>
2023-04-10 05:10:01 +0600

    banner section is completed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className='grid lg:grod-cols-2'>
-            <div className="banner-details">
-
+        <div className='flex lg:flex-row  container mx-auto my-10 items-center flex-col-reverse'>
+            <div className="banner-details w-full lg:w-2/5">
+                <h1 className='text-2xl  lg:text-6xl font-extrabold text-black mt-6 lg:mt-20 mb-6'>One Step <br /> Closer To Your <br /><span className='text-cyan-600'>Dream Job</span></h1>
+                <p className='mb-8'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                <button className='btn-primary'>Get Started</button>
             </div>
-            <div className="banner-image">
-                <img src="" alt="" />
+            <div className="banner-image w-3/4 mt-10">
+                <img src="../../../public/P3OLGJ1 copy 1.png" className='w-full' alt="" />
             </div>
         </div>
     );
