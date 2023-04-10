39e963ad6ef0ff01c9cc69a547d3ba32251717ec
sumon2807 <sharifulbd911@gmail.com>
2023-04-10 06:26:13 +0600

    category section UI design is completed

---
+++ src/components/Featured/Featured.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Featured = () => {
+    return (
+        <div className='container mx-auto'>
+            <div className='text-center mb-10'>
+                <h1 className='text-5xl font-extrabold mt-32'>Featured Jobs</h1>
+                <p className='font-medium text-base text-slate-600'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+        </div>
+    );
+};
+
+export default Featured;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import Category from '../Category/Category';
+import Featured from '../Featured/Featured';
 
 const Home = () => {
     return (
-        <div className='flex lg:flex-row  container mx-auto my-10 items-center flex-col-reverse'>
-            <div className="banner-details w-full lg:w-2/5">
-                <h1 className='text-2xl  lg:text-6xl font-extrabold text-black mt-6 lg:mt-20 mb-6'>One Step <br /> Closer To Your <br /><span className='text-cyan-600'>Dream Job</span></h1>
-                <p className='mb-8'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
-                <button className='btn-primary'>Get Started</button>
+        <div>
+            <div className='flex lg:flex-row  container mx-auto my-10 items-center flex-col-reverse'>
+                <div className="banner-details w-full lg:w-2/5">
+                    <h1 className='text-2xl  lg:text-6xl font-extrabold text-black mt-6 lg:mt-20 mb-6'>One Step <br /> Closer To Your <br /><span className='text-cyan-600'>Dream Job</span></h1>
+                    <p className='mb-8 text-base text-slate-600'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                    <button className='btn-primary'>Get Started</button>
+                </div>
+                <div className="banner-image w-3/4 mt-10">
+                    <img src="../../../public/All Images/P3OLGJ1 copy 1.png" className='w-full' alt="" />
+                </div>
             </div>
-            <div className="banner-image w-3/4 mt-10">
-                <img src="../../../public/P3OLGJ1 copy 1.png" className='w-full' alt="" />
-            </div>
+            <Category></Category>
+            <Featured></Featured>
         </div>
     );
 };
