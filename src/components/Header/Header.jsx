32a4dae72c45226a09488dd852ecb0461d9d29f3
sumon2807 <sharifulbd911@gmail.com>
2023-04-11 19:42:48 +0600

    manubar toggole icon is added and make it responsive also

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { Bars3Icon, XMarkIcon} from '@heroicons/react/24/solid'
 
 const Header = () => {
+    const [open, setOpen]=useState(false)
     return (
         <div className='flex justify-between items-center py-4 container mx-auto'>
             <div>
-                <Link to='/' className='text-3xl font-bold'>Job<span className='text-cyan-600'>Finder</span></Link>
+                <Link to='/' className='text-3xl font-bold pl-4 lg:p-0 md:p-0'>Job<span className='text-cyan-600'>Finder</span></Link>
             </div>
             <div>
-                <ul className='flex font-bold mt-2'>
+            <div className='md:hidden lg:hidden absolute right-0 pr-4' onClick={()=>setOpen(!open)}>
+                <span>{open=== true ? <XMarkIcon className="h-6 w-6 text-blue-500" /> : <Bars3Icon className="h-6 w-6 text-blue-500" />}</span>
+            
+            
+            </div>
+                <ul className={`md:flex lg:flex font-bold mt-2 absolute md:static lg:static duration-500 ${open ? 'top-6' : '-top-48'}`}>
                     <li className='mx-4'>
                         <NavLink to='/' className={({isActive})=>(isActive ? 'text-cyan-600' : 'default')}>Home</NavLink>
                     </li>
@@ -24,7 +31,7 @@ const Header = () => {
                 </ul>
             </div>
             <div>
-                <button className='btn-primary'>Star Applying</button>
+                <button className='btn-primary invisible md:visible lg:visible'>Star Applying</button>
             </div>
         </div>
     );
